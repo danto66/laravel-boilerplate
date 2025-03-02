455cf6da3ff122cd830fd1273b1bb89624ec2634
Danang Ardianto <ardhyanto6629@gmail.com>
2025-03-02 13:21:34 +0700

    refactor search handling to return early when no keyword is provided and simplify timestamp attribute methods

--- app/Models/Traits/WithUnixTimestamps.php
+++ app/Models/Traits/WithUnixTimestamps.php
@@ -10,16 +10,14 @@ trait WithUnixTimestamps
     protected function createdAtUnix(): Attribute
     {
         return Attribute::make(
-            get: fn($_, $attributes) => Carbon::parse($attributes['created_at'])->unix(),
-            set: fn($_, $attributes) => Carbon::createFromTimestamp($attributes['created_at']),
+            get: fn($_, $attributes) => Carbon::parse($attributes['created_at'])->unix()
         );
     }
 
     protected function updatedAtUnix(): Attribute
     {
         return Attribute::make(
-            get: fn($_, $attributes) => Carbon::parse($attributes['updated_at'])->unix(),
-            set: fn($_, $attributes) => Carbon::createFromTimestamp($attributes['updated_at']),
+            get: fn($_, $attributes) => Carbon::parse($attributes['updated_at'])->unix()
         );
     }
 }
